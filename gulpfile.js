4b19410b3374912969c70d748eceacfa81b6350c
luizsantos <luiz.ssantos@outlook.com.br>
2025-01-15 06:31:03 -0300

    Criando Hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
+// Tarefa para compilar arquivos SCSS
 function styles() {
   return gulp
     .src("./src/styles/*.scss")
@@ -8,7 +10,19 @@ function styles() {
     .pipe(gulp.dest("./dist/css"));
 }
 
-exports.default = styles;
-exports.watch = function () {
-  gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
-};
+// Tarefa para otimizar imagens
+function images() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));
+}
+
+// Tarefa para assistir mudanças em arquivos
+function watch() {
+  return gulp.watch("./src/styles/*.scss", styles);
+}
+
+// Exportando as tarefas
+exports.default = gulp.parallel(styles, images);
+exports.watch = watch;
